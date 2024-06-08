a020d44560c6fcb5db87de052b20a227bc065421
yogesh3256 <yogeshwakchaure3256@gmail.com>
2024-06-08 17:15:39 +0530

    map the nested array and manipulate the array

--- src/Component/nestedArrayTask/ArrayMapTask.js
+++ src/Component/nestedArrayTask/ArrayMapTask.js
@@ -1,34 +1,83 @@
-import React, { useEffect, useState } from 'react';
+import React, { memo, useState } from 'react';
 import Accordion from '@mui/material/Accordion';
 import AccordionSummary from '@mui/material/AccordionSummary';
 import AccordionDetails from '@mui/material/AccordionDetails';
 import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
+import ArrowDropDownIcon from '@mui/icons-material/ArrowDropDown';
+import { styled } from '@mui/material/styles';
+import FormControlLabel from '@mui/material/FormControlLabel';
+import Switch from '@mui/material/Switch';
 import { DummyData } from './DummyArray';
+
+const IOSSwitch = styled((props) => (
+    <Switch focusVisibleClassName=".Mui-focusVisible" disableRipple {...props} />
+))(({ theme }) => ({
+    width: 42,
+    height: 26,
+    padding: 0,
+    '& .MuiSwitch-switchBase': {
+        padding: 0,
+        margin: 2,
+        transitionDuration: '300ms',
+        '&.Mui-checked': {
+            transform: 'translateX(16px)',
+            color: '#fff',
+            '& + .MuiSwitch-track': {
+                backgroundColor: theme.palette.mode === 'dark' ? '#2ECA45' : '#65C466',
+                opacity: 1,
+                border: 0,
+            },
+            '&.Mui-disabled + .MuiSwitch-track': {
+                opacity: 0.5,
+            },
+        },
+        '&.Mui-focusVisible .MuiSwitch-thumb': {
+            color: '#33cf4d',
+            border: '6px solid #fff',
+        },
+        '&.Mui-disabled .MuiSwitch-thumb': {
+            color:
+                theme.palette.mode === 'light'
+                    ? theme.palette.grey[100]
+                    : theme.palette.grey[600],
+        },
+        '&.Mui-disabled + .MuiSwitch-track': {
+            opacity: theme.palette.mode === 'light' ? 0.7 : 0.3,
+        },
+    },
+    '& .MuiSwitch-thumb': {
+        boxSizing: 'border-box',
+        width: 22,
+        height: 22,
+    },
+    '& .MuiSwitch-track': {
+        borderRadius: 26 / 2,
+        backgroundColor: theme.palette.mode === 'light' ? '#E9E9EA' : '#39393D',
+        opacity: 1,
+        transition: theme.transitions.create(['background-color'], {
+            duration: 500,
+        }),
+    },
+}));
 
 function ArrayMapTask() {
     const [dummyArrayData, setDummyArrayData] = useState(DummyData.subFunction);
     const [expanded, setExpanded] = useState(Array(DummyData.subFunction.length).fill(false));
-
-    useEffect(() => {
-        console.log("modified dataArray is :", dummyArrayData);
-    }, [dummyArrayData]);
 
     const handleParentChange = (e, index) => {
         e.stopPropagation(); // Prevent the accordion from closing
         let data = [...dummyArrayData];
         data[index].isChecked = e.target.checked;
 
-        if (data[index]?.subFunction && data[index].subFunction.length > 0) {
-            // Check or uncheck all child checkboxes
-            for (let functionObject of data[index].subFunction) {
-                functionObject.isChecked = e.target.checked;
-            }
-        }
-
-        // Check if all child checkboxes are checked
-        const allChecked = data[index].subFunction.every(child => child.isChecked);
-        if (allChecked) {
-            data[index].isChecked = true;
+        if (data[index]?.subFunction) {
+            data[index].subFunction = data[index].subFunction.map(sub => ({
+                ...sub,
+                isChecked: e.target.checked,
+                permissions: sub.permissions.map(permission => ({
+                    ...permission,
+                    isChecked: e.target.checked
+                }))
+            }));
         }
 
         setDummyArrayData(data);
@@ -39,16 +88,40 @@ function ArrayMapTask() {
         let data = [...dummyArrayData];
         data[parentIndex].subFunction[childIndex].isChecked = e.target.checked;
 
-        // Check if all child checkboxes are checked
-        const allChecked = data[parentIndex].subFunction.every(child => child.isChecked);
-        if (allChecked) {
-            data[parentIndex].isChecked = true;
-        } else {
-            data[parentIndex].isChecked = false;
+        if (data[parentIndex].subFunction[childIndex]?.permissions) {
+            data[parentIndex].subFunction[childIndex].permissions = data[parentIndex].subFunction[childIndex].permissions.map(permission =>
+                 ({
+                ...permission,
+                isChecked: e.target.checked
+            }));
         }
+
+        // Check if all child checkboxes are unchecked
+        const allUnchecked = data[parentIndex].subFunction.every(child => !child.isChecked);
+        data[parentIndex].isChecked = !allUnchecked;
 
         setDummyArrayData(data);
     };
+
+    const handlePermissionChange = (e, parentIndex, childIndex, permissionIndex) => {
+        let data = [...dummyArrayData];
+        data[parentIndex].subFunction[childIndex].permissions[permissionIndex].isChecked = e.target.checked;
+    
+        // Check if all permission checkboxes are unchecked
+        const allPermissionsUnchecked = data[parentIndex].subFunction[childIndex].permissions.every(permission => !permission.isChecked);
+        data[parentIndex].subFunction[childIndex].isChecked = !allPermissionsUnchecked;
+    
+        // Check if all child checkboxes are checked
+        const allChildrenChecked = data[parentIndex].subFunction.every(child => child.isChecked);
+        data[parentIndex].isChecked = allChildrenChecked;
+    
+        // Check if all child checkboxes are unchecked
+        const allChildrenUnchecked = data[parentIndex].subFunction.every(child => !child.isChecked);
+        data[parentIndex].isChecked = !allChildrenUnchecked;
+    
+        setDummyArrayData(data);
+    };
+    
 
     const handleAccordionChange = (index) => (event, isExpanded) => {
         setExpanded(expanded.map((exp, i) => (i === index ? isExpanded : exp)));
@@ -63,56 +136,95 @@ function ArrayMapTask() {
                             expanded={expanded[parentIndex]}
                             onChange={handleAccordionChange(parentIndex)}
                             sx={{
-                                backgroundColor: '##C0C0C01', 
-                                color: '#333', 
+                                backgroundColor: 'white',
+                                color: '#333',
                                 '&:before': {
                                     display: 'none',
                                 },
-                                borderRadius: '5px',  
-                                marginBottom: '5px',  
-                                boxShadow: '0px 2px 8px rgba(0,0,0,0.1)',  
-                                overflow: 'hidden',  
+                                borderRadius: '5px',
+                                marginBottom: '5px',
+                                boxShadow: '0px 2px 8px rgba(0,0,0,0.1)',
+                                overflow: 'hidden',
                             }}
                         >
                             <AccordionSummary
                                 expandIcon={<ExpandMoreIcon />}
                                 aria-controls="panel1-content"
                                 id="panel1-header"
-                                sx={{
-                                    backgroundColor: '#d3d3d3',  
-                                    '&.Mui-expanded': {
-                                        minHeight: '50px',
-                                    },
-                                }}
+                                onClick={(e) => e.stopPropagation()} // Prevent accordion from closing on checkbox click
                             >
                                 <div className='flex gap-4'>
                                     <input
                                         type='checkbox'
                                         checked={data.isChecked}
-                                        onClick={(e) => e.stopPropagation()} // Prevent accordion from closing on checkbox click
                                         onChange={(e) => handleParentChange(e, parentIndex)}
                                     />
-                                    <label className='font-semibold tracking-wider'>{data.functionality}</label>
+                                    <label className='font-bold tracking-wider'>{data.functionality}</label>
                                 </div>
                             </AccordionSummary>
                             <AccordionDetails
                                 sx={{
-                                    backgroundColor: '#fff',  
+                                    backgroundColor: '#fff',
                                 }}
                             >
-                                <div className='grid grid-cols-5 gap-4'>
-                                    {data?.["subFunction"] && data.subFunction.length > 0 &&
-                                        data?.["subFunction"].map((subFunctionData, childIndex) => (
-                                            <div key={childIndex} className='flex gap-3'>
-                                                <input
-                                                    type='checkbox'
-                                                    checked={subFunctionData.isChecked}
-                                                    onChange={(e) => handleChildChange(e, parentIndex, childIndex)}
-                                                />
-                                                <label>{subFunctionData.functionality}</label>
-                                            </div>
-                                        ))}
-                                </div>
+                                {data?.subFunction && data.subFunction.length > 0 &&
+                                    data.subFunction.map((subFunctionData, childIndex) => (
+                                        <div key={childIndex} className='ml-10 mb-3'>
+                                            <Accordion
+                                            
+                                            >
+                                                <AccordionSummary
+                                                    expandIcon={<ArrowDropDownIcon />}
+                                                    aria-controls="panel2-content"
+                                                    id="panel2-header"
+                                                    sx={{
+                                                        backgroundColor: 'white',
+                                                        color: '#333',
+                                                        '&:before': {
+                                                            display: 'none',
+                                                        },
+                                                        borderRadius: '5px',
+                                                        marginBottom: '5px',
+                                                        boxShadow: '0px 2px 8px rgba(0,0,0,0.1)',
+                                                        overflow: 'hidden',
+                                                    }}
+                                                >
+                                                    <div className='flex gap-3'>
+                                                        <input
+                                                            type='checkbox'
+                                                            checked={subFunctionData.isChecked}
+                                                            onChange={(e) => handleChildChange(e, parentIndex, childIndex)}
+                                                        />
+                                                        <label className='font-semibold tracking-wide'>{subFunctionData.functionality}</label>
+                                                    </div>
+                                                </AccordionSummary>
+                                               <div className='pl-20'>
+                                               <AccordionDetails>
+                                                    {subFunctionData?.permissions && subFunctionData.permissions.length > 0 &&
+                                                        <div className='grid grid-cols-3 gap-3'>
+                                                            {subFunctionData.permissions.map((permissionData, permissionIndex) => (
+                                                                <div key={permissionIndex} className='grid grid-cols-2'>
+                                                                    <div className='flex gap-3'>
+                                                                        <input
+                                                                            type='checkbox'
+                                                                            checked={permissionData.isChecked}
+                                                                            onChange={(e) => handlePermissionChange(e, parentIndex, childIndex, permissionIndex)}
+                                                                        />
+                                                                        <h1 className='mt-2 font-mono'>{permissionData.permission}</h1>
+                                                                    </div>
+                                                                    <FormControlLabel
+                                                                        control={<IOSSwitch sx={{ m: 1 }}
+                                                                            checked={permissionData.isAction} />}
+                                                                    />
+                                                                </div>
+                                                            ))}
+                                                        </div>
+                                                    }
+                                                </AccordionDetails>
+                                               </div>
+                                            </Accordion>
+                                        </div>
+                                    ))}
                             </AccordionDetails>
                         </Accordion>
                     </div>
@@ -120,5 +232,4 @@ function ArrayMapTask() {
         </>
     );
 }
-
-export default ArrayMapTask;
+export default memo(ArrayMapTask);
---
+++ src/Component/practice Task/codingRoundQst/ReusebleInputQst.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function ReusebleInputQst() {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default ReusebleInputQst
